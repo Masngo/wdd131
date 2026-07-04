36919e2a77753ba473f9ce4b2afd775c8e1ea3c8
Masango763 <dewhemasango@gmail.com>
2026-07-05 00:38:28 +0200

    Fix image paths and add CSS grid

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
-const yearSpan = document.getElementById("currentyear");
-const lastModified = document.getElementById("lastModified");
+// Dynamically set the current year for the copyright notice
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-yearSpan.textContent = new Date().getFullYear();
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
+// Dynamically set the last modified date of the document
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
