3dcf5b4b412e915ae288afceab977f503f16ef86
Masngo <m.dewhe@gmail.com>
2026-07-04 05:27:53 +0200

    W01 completed exercises

---
+++ Downloads/cse210-ww-student-template-main/cse210-ww-student-template-main/week01/Exercise3/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess = -1;
+
+        Console.WriteLine("Guess My Number Game");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+                Console.WriteLine("Higher");
+            else if (guess > magicNumber)
+                Console.WriteLine("Lower");
+            else
+                Console.WriteLine("You guessed it!");
+        }
+    }
+}
