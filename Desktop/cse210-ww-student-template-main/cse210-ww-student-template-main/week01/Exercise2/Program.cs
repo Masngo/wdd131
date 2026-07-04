08f7f42e39f581b7ee2cecca0b67d5fb66e7e92c
Masngo <m.dewhe@gmail.com>
2026-07-04 13:11:49 +0200

    Clean initial commit

---
+++ Desktop/cse210-ww-student-template-main/cse210-ww-student-template-main/week01/Exercise2/Program.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your grade percentage? ");
+        int percent = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (percent >= 90)
+            letter = "A";
+        else if (percent >= 80)
+            letter = "B";
+        else if (percent >= 70)
+            letter = "C";
+        else if (percent >= 60)
+            letter = "D";
+        else
+            letter = "F";
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+            Console.WriteLine("Congratulations, you passed!");
+        else
+            Console.WriteLine("Keep trying, you can improve!");
+    }
+}
