753a99ce77643661fe8d76fe997825968becd4cd
Masango763 <dewhemasango@gmail.com>
2026-07-04 23:50:09 +0200

    WDD131 Week 01 complete project

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
-const year = document.getElementById("currentyear");
+const yearSpan = document.getElementById("currentyear");
 const lastModified = document.getElementById("lastModified");
 
-year.textContent = new Date().getFullYear();
-lastModified.textContent = document.lastModified;
+yearSpan.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
